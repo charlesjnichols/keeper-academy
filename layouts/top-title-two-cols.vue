8f7af7332310079ca76f4bdb76ac54cc03ecd57d
cjnicho <charlesjnichols@gmail.com>
2026-08-07 15:50:43 -0400

    adding back in missing ::default::

--- layouts/top-title-two-cols.vue
+++ layouts/top-title-two-cols.vue
@@ -50,6 +50,14 @@ const marginClass = computed(() => {
       </div>
     </div>
 
+    <!-- Optional full-width default slot -->
+
+    <div v-if="$slots.default" class="w-full h-fit flex-shrink-0 px-8">
+      <div class="slidev-layout default w-full m-0" style="padding-bottom: 0 !important;">
+        <slot name="default" />
+      </div>
+    </div>
+
     <!-- Two Columns Content Area (Takes up remaining height & enables scrollbars) -->
     <div class="grid grid-cols-2 gap-8 flex-1 min-h-0 p-8">
       <!-- Left Column with Scroll Bar -->
